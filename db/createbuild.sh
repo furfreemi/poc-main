1887f4a942bfc80d2fc678829cae1b3e990134ec
Larissa Marcich <lmarcich@thoughtworks.com>
2017-05-09 16:52:16 -0400

    [LM] further consolidates specific database details

--- db/createbuild.sh
+++ db/createbuild.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-function createdb() {
-  DATABASE=$1
-  USER=root
-  PASSWORD=rootpass
-  mysql -u$USER -p$PASSWORD -f -e "DROP DATABASE IF EXISTS ${DATABASE};"
-  mysqladmin -u$USER -p$PASSWORD create $DATABASE
-  mysql -u$USER -p$PASSWORD $DATABASE < backup.sql;
-}
+DATABASE=$1
+USER=$2
+PASSWORD=$3
+mysql -u${USER} -p${PASSWORD} -f -e "DROP DATABASE IF EXISTS ${DATABASE};"
+mysqladmin -u${USER} -p${PASSWORD} create ${DATABASE}
+mysql -u${USER} -p${PASSWORD} ${DATABASE} < backup.sql;
 
-createdb "wkh_poc"
+
+
--- src/main/java/com/tw/wkh/filewrite/DatabaseConnectionUtil.java
+++ src/main/java/com/tw/wkh/filewrite/DatabaseConnectionUtil.java
@@ -1,7 +1,6 @@
 package com.tw.wkh.filewrite;
 
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
